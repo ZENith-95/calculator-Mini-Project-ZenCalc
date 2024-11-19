4cf40a400b47d14c3f7f8c2c1fcb0cf44b328066
ZENith-95 <dz.korbla@gmail.com>
2024-11-19 17:44:03 +0000

    added javascript functionality and  finishing touches

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const input = document.querySelector("#input-disp")
+const inputElement = document.querySelector("#input-disp");
+const resultElement = document.querySelector("#result-disp");
 
-function addInput (input){
-    input.value += input;
-
+function addInput(newValue) {
+  inputElement.value += newValue;
 }
 
-function clearDisplay(){
+function clearInput() {
+  inputElement.value = inputElement.value.slice(0, -1);
+}
 
+function clearDisplay() {
+  inputElement.value = "";
+  resultElement.value = "";
+}
+
+function calculate() {
+  try {
+    resultElement.value = eval(inputElement.value);
+  } catch (error) {
+    resultElement.value = "Error";
+  }
 }
